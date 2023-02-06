8b7c1605bd7ff6d7cbf6dc52156f5f97579a66b4
ifqygazhar <ifqygazhar@gmail.com>
2023-02-06 17:36:07 +0700

    first commit

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayHello("ifqy"));
+
+  var customer = new Customer();
+  var category = new Category();
+  var product = new Product();
+}
---
+++ lib/src/customer.dart
@@ -0,0 +1,5 @@
+class Customer {}
+
+class Product {}
+
+class Category {}
